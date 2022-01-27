bd9ebfbebb122196ef566e277fedba04b7fd052c
howardt12345 <howardt12345@gmail.com>
2022-01-26 23:23:04 -0500

    logo swap

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/logo.png",
       },
     },
     "gatsby-plugin-mdx",

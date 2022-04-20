7f4919ca0bc2f8e0581028e2df0aceb5353b4b69
howardt12345 <howardt12345@gmail.com>
2022-04-19 22:29:27 -0400

    middleware with correct path

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,7 +2,7 @@ import { NextResponse, NextRequest } from 'next/server';
 export async function middleware(req: { nextUrl: { pathname: any } }, ev: any) {
   const { pathname } = req.nextUrl;
   if (pathname == '/') {
-    return NextResponse.redirect('/hello-nextjs');
+    return NextResponse.redirect('/designs?home=true');
   }
   return NextResponse.next();
 }
